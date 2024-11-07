7b7fb73f23d7119664f8509de499d3f109f29dfe
mohamed <mr319242@gmail.com>
2024-11-07 23:11:15 +0200

    update new

---
+++ database/migrations/2024_11_07_200953_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('freelancer_id')->nullable();
+            $table->string('title');
+            $table->text('desc');
+            $table->double('price','8','2');
+            $table->text('files');
+            $table->text('skills');
+            $table->string('day_number');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
